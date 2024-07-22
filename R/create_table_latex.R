c30e2056badc3c3aea1ffb922b9f74c49d8b48ea
Beni Stocker <benjamin.stocker@gmail.com>
2024-07-22 16:11:42 +0200

    revised descriptions

--- R/create_table_latex.R
+++ R/create_table_latex.R
@@ -14,6 +14,6 @@ create_table_latex <- function( df, caption, filn = "", ... ){
 	  ... 
 	  )
 	
-  return(NULL)
+  return(filn)
 }
 
--- R/process_cite_lines.R
+++ R/process_cite_lines.R
@@ -1,17 +1,16 @@
-library(stringr)
-
 # Function to process lines containing '$\backslash$cite'
-process_cite_lines <- function(text) {
+process_cite_lines <- function(text){
+    
   # Define the regex pattern to detect lines with '$\backslash$cite'
   pattern <- "\\$\\\\backslash\\$cite"
   
   # Process each line
   processed_lines <- sapply(text, function(line) {
-    if (str_detect(line, pattern)) {
+    if (stringr::str_detect(line, pattern)) {
       cat("Original line:", line, "\n")
       
       # Find the position of '$\backslash$cite'
-      cite_pos <- str_locate(line, pattern)[1, ]
+      cite_pos <- stringr::str_locate(line, pattern)[1, ]
       before_cite <- substr(line, 1, cite_pos[2])
       after_cite <- substr(line, cite_pos[2] + 1, nchar(line))
       
